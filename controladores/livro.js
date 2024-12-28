7830adaf5c13e7c94d7eb5fb061853ad2edcdb59
Rafael Zipão Silva <zipao.silva@gmail.com>
2024-12-27 21:23:51 -0300

    aula4

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,4 +1,4 @@
-const { getTodosLivros } = require('../servicos/livro');
+const { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletarLivroPorId } = require('../servicos/livro');
 
 function getLivros(req, res) {
     try {
@@ -11,6 +11,59 @@ function getLivros(req, res) {
     }
 };
 
+function getLivro(req, res) {
+    try {
+        //throw new Error (''); //- comando para forçar um erro para testar o catch
+        const id = req.params.id;
+        const livro = getLivroPorId(id);
+        res.send(livro);
+    } catch (error) {
+        res.status(500);
+        res.send({error:error.Error()});
+    }
+};
+
+function postLivro(req, res) {
+    try {
+        const livroNovo = req.body;
+        insereLivro(livroNovo);
+        res.status(201);
+        res.send('Livro inserido com sucesso');
+    } catch(error) {
+        res.status(500);
+        res.send(error.message);
+    }
+}
+
+function patchLivro(req, res) {
+    try {
+        const id = req.params.id;
+        const body = req.body;
+
+        modificaLivro(body,id);
+        res.send(`Item de ID nº ${id} modificado com sucesso`)
+
+    } catch(error) {
+        res.status(500);
+        res.send(error.message);
+    } 
+}
+
+function deleteLivro(req, res) {
+    try {
+        const id = req.params.id;
+        deletarLivroPorId(id);
+        res.send(`Item de ID nº ${id} removido com sucesso`)
+
+    } catch(error) {
+        res.status(500);
+        res.send(error.message);
+    } 
+}
 module.exports = {
-    getLivros
+    getLivros,
+    getLivro,
+    postLivro,
+    patchLivro,
+    deleteLivro
 }
--- rotas/livro.js
+++ rotas/livro.js
@@ -1,38 +1,16 @@
 const { Router } = require('express');
-const { getLivros } = require('../controladores/livro');
+const { getLivros, getLivro, postLivro, patchLivro, deleteLivro } = require('../controladores/livro');
 
 const router = Router();
 
 router.get('/', getLivros);
 
-router.post('/', (req, res) => {
-    try {
-        //throw new Error (''); //- comando para forçar um erro para testar o catch
-        res.send('Você fez uma requisição do tipo POST');
-    } catch (Error) {
-        res.status(500);
-        res.send(Error.message);
-    }
-});
+router.get('/id=:id', getLivro);
 
-router.patch('/', (req, res) => {
-    try {
-        //throw new Error (''); //- comando para forçar um erro para testar o catch
-        res.send('Você fez uma requisição do tipo PATCH');
-    } catch (Error) {
-        res.status(500);
-        res.send(Error.message);
-    }   
-});
+router.post('/', postLivro);
 
-router.delete('/', (req, res) => {
-    try {
-        //throw new Error (''); //- comando para forçar um erro para testar o catch
-        res.send('Você fez uma requisição do tipo DELETE');
-    } catch (Error) {
-        res.status(500);
-        res.send(Error.message);
-    }
-});
+router.patch('/id=:id', patchLivro);
+
+router.delete('/id=:id', deleteLivro);
 
 module.exports = router;
