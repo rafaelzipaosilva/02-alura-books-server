497ce45db838d6d7d80821a348d5ee41b72b2eca
Rafael Zipão Silva <zipao.silva@gmail.com>
2025-01-16 12:54:56 -0300

    aula3

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+## Comandos
+nodemon app.js //rodar a API
+
 npm install cors //install o cors para evitar erros na requisição http
--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const express = require("express")
-const rotaLivro = require("./rotas/livro")
-const cors = require("cors")
+const express = require('express')
+const rotaLivro = require('./rotas/livro')
+const rotaFavorito = require('./rotas/favorito');
+
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
-app.use(cors({origin: "*"}))
+app.use(cors({origin: '*'}))
 
 app.use('/livros', rotaLivro)
+app.use('/favoritos', rotaFavorito)
 
 const port = 8000
 
